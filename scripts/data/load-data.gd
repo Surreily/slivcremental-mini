46b01652a91ffa8782bf9af2105dbb2aa83a96b9
Sean Reilly <surreily1@gmail.com>
2026-01-01 20:15:35 +0000

    Add a basic colourise shader and some other stuff.

---
+++ scripts/data/load-data.gd
@@ -0,0 +1,4 @@
+class_name LoadData extends Node
+
+func _load_data() -> void:
+	# bog
---
+++ scripts/data/skills.gd
@@ -0,0 +1,12 @@
+extends Node
+
+# Incremental growth points skills.
+var isIncrementalGrowthPoints1Unlocked: bool = false
+var isIncrementalGrowthPoints2Unlocked: bool = false
+var isIncrementalGrowthPoints3Unlocked: bool = false
+var isIncrementalGrowthPoints4Unlocked: bool = false
+var isIncrementalGrowthPoints5Unlocked: bool = false
+var isIncrementalGrowthPoints6Unlocked: bool = false
+
+# Camera zoom skill.
+var isCameraZoomUnlocked: bool = false

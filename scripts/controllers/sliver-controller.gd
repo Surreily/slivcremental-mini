45c9ba36c57f6b1d91ef0d1e1d08bc3494b9cc79
Sean Reilly <surreily1@gmail.com>
2026-02-01 22:21:43 +0000

    Add visual "pulse" when sliv point is gained.

--- scripts/controllers/sliver-controller.gd
+++ scripts/controllers/sliver-controller.gd
@@ -6,6 +6,8 @@ var sliver: Sliver
 var sliv_point_progress: float = 0
 var sliv_point_target: float = 10
 
+var sliv_point_gained_tween: Tween
+
 var random: RandomNumberGenerator
 
 func _ready() -> void:
@@ -23,3 +25,10 @@ func _process_sliv(delta: float) -> void:
 		State.currencies.sliv_points += State.statistics.growthPointsPerClick
 		sliv_point_progress = 0.0
 		sliv_point_target = random.randf_range(3.0, 15.0)
+		
+		if (sliv_point_gained_tween != null):
+			sliv_point_gained_tween.stop()
+			
+		scale = Vector2(1.2, 1.2)
+		sliv_point_gained_tween = get_tree().create_tween()
+		sliv_point_gained_tween.tween_property(self, "scale", Vector2.ONE, 0.25)
